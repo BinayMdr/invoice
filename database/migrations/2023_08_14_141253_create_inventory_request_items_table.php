9847c447503eb41f196de8885743f231c9858336
Binay Manandhar <binay.manandhar@amniltech.com>
2023-08-14 20:01:54 +0545

    Table set up for inventory request

---
+++ database/migrations/2023_08_14_141253_create_inventory_request_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory_request_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('inventory_request_id');
+            $table->integer('inventory_id');
+            $table->float('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory_request_items');
+    }
+};
